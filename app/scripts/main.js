67b747d3bf28c4c92aec5fcb994df2734d2d9de3
Josh Daniels <daniels.joshua100@gmail.com>
2014-07-07 23:34:36 -0400

    setup testing suite

---
+++ app/scripts/main.js
@@ -0,0 +1,12 @@
+var pillow = [{material: 'feathers', hardness: 10}, {material: 'foam', hardness: 30}, {material: 'rice', hardness: 60}];
+var material_value = _.pluck(pillow, 'material');
+
+
+
+var last_one = _.last(["cat", "dog", "turtle", "frog"]);
+
+
+var first_one = _.first(["cat", "dog", "turtle", "frog"]);
+
+
+var random_number = _.random(0,10);
